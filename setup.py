f1e92703fc5f2e194ae2e287b2f6fff46d09c7ff
Doug Hellmann <doug@doughellmann.com>
2016-08-23 13:18:42 -0400

    Fix description metadata
    There doesn't seem to be a "summary" option, but "description" and
    "long_description" are different. Update the metadata to avoid warnings
    about the unknown "summary" and so that when the release announce script
    asks for the package description it gets the one line version instead of
    the longer one.
    
    Also, bump the build number as 1.4.13.1 has been released.
    
    Change-Id: Ibe475d90c97f6e0c09be92c211924546d7d8cca1
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ long_description = open('README.txt').read()
 
 setup(
     name='XStatic-smart-table',
-    summary="""smart-table 1.4.13 (XStatic packaging standard)""",
-    description=long_description,
+    description="""smart-table 1.4.13 (XStatic packaging standard)""",
+    long_description=long_description,
     maintainer="Richard Jones",
     maintainer_email='r1chardj0n3s@gmail.com',
     use_scm_version=True,
